ba4cb8e5b82c09e8dc35199138feeeabac7439c8
Eduardo Torres <contato@eduardotorres.dev>
2023-07-12 01:07:47 -0300

    feat: open graph tags

---
+++ src/routes/home/Glossario.svelte
@@ -0,0 +1,38 @@
+<script>
+	const letras = Array.from({ length: 26 }, (_, index) =>
+		String.fromCharCode(65 + index)
+	);
+</script>
+
+<section class="glossario">
+	<div class="glossario__toolbar">
+		<ul class="glossario__letras">
+			{#each letras as letra}
+				<li class="glossario__letra">{letra}</li>
+			{/each}
+		</ul>
+	</div>
+    <div class="glossario__body">
+        <div class="glossario__header">
+            <input type="text" class="glossario__busca">
+            <ul class="glossario__grupos">
+                <li class="glossario__grupo">
+                    <span class="glossario__categoria">
+                        
+                    </span>
+                    <ul class="glossario__palavras">
+                        <li class="glossario__palavra">
+
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="glossario__description">
+            <p></p>
+        </div>
+        <span class="glossario__title">
+            GLOSSÁRIO
+        </span>
+    </div>
+</section>
