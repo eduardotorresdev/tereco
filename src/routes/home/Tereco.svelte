e9853b495e1282b4493a45ab55fbb445024edc93
Eduardo Torres <contato@eduardotorres.dev>
2023-07-11 22:41:47 -0300

    fix: remove dependencias suspeitas

--- src/routes/home/Tereco.svelte
+++ src/routes/home/Tereco.svelte
@@ -11,23 +11,25 @@
 				aconteciam nos espaços mais afastados, adentrando as matas codoenses
 				para não serem descobertos.
 			</p>
+			<p>
+				Em Codó-MA, foi possível que as palavras terecô e umbanda foram usados
+				como sinônimos para denominar a religião nascida nas matas codoenses. A
+				religião afro-brasileira terecô tem como patrona a entidade Barba
+				Soeiro, por esse motivo é padroeira de várias casas de culto do terecô.
+				A base ritualística do terecô está ligada à encantaria. A encantaria é
+				um processo místico em que pessoas somem e são rebatizadas para ajudar
+				os pecadores na terra. Os encantados são seres mais evoluídos que descem
+				para ajudar as pessoas pela intervenção dos médiuns. No mundo dos
+				encantados, não há pecadores e nem santos, os encantados ficam entre o
+				céu e a terra.
+			</p>
 		</div>
-        <ul class="tereco__termos">
-            <li class="tereco__termo">
-                ___ PIFE
-            </li>
-            <li class="tereco__termo">
-                ENCANTADO
-            </li>
-            <li class="tereco__termo">
-                BABÁ
-            </li>
-            <li class="tereco__termo">
-                _VINHANÇA_
-            </li>
-            <li class="tereco__termo">
-                MANJÁ__
-            </li>
-        </ul>
+		<ul class="tereco__termos">
+			<li class="tereco__termo">___ PIFE</li>
+			<li class="tereco__termo">ENCANTADO</li>
+			<li class="tereco__termo">BABÁ</li>
+			<li class="tereco__termo">_VINHANÇA_</li>
+			<li class="tereco__termo">MANJÁ__</li>
+		</ul>
 	</div>
 </section>
