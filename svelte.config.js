38134489410637c6690d554d9f6a05ff38b9b90d
Eduardo Torres <contato@eduardotorres.dev>
2023-07-11 22:44:42 -0300

    fix: remove plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { sveltekit } from '@sveltejs/kit/vite'
-import svg from '@poppanator/sveltekit-svg'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
-	plugins: [
-		sveltekit(),
-		svg(),
-	],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
