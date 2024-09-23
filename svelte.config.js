f550d61b59c4ce5716e677c0e2dfb2667bfb15d0
Eduardo Torres <contato@eduardotorres.dev>
2024-09-22 21:56:00 -0300

    feat: adiciona o dockerfile de deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown
 			out: 'build',
 			precompress: false,
 			envPrefix: '',
-			polyfill: true
+			polyfill: true,
+			sourcemap: true,
 		}),
 
 		alias: {
